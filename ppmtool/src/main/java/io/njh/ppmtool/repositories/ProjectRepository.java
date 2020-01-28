a08ca5a95782f206b550959aa2b1cb754eceebc6
Nate <github email address>
2020-01-28 18:45:46 -0500

    Project repository class

---
+++ ppmtool/src/main/java/io/njh/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.njh.ppmtool.repositories;
+
+import io.njh.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
