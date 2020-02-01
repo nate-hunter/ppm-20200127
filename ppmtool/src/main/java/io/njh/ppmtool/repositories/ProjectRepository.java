52ff5be4e4a11fa13e41306ffc1d017d48461260
Nate <github email address>
2020-01-31 19:46:44 -0500

    Project exceptions + GET Project

--- ppmtool/src/main/java/io/njh/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/io/njh/ppmtool/repositories/ProjectRepository.java
@@ -9,4 +9,7 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
 
     @Override
     Iterable<Project> findAllById(Iterable<Long> iterable);
+
+    Project findByProjectIdentifier(String projectId);
+
 }
--- ppmtool/src/main/java/io/njh/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/njh/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.njh.ppmtool.services;
 
 import io.njh.ppmtool.domain.Project;
+import io.njh.ppmtool.exceptions.ProjectIdException;
 import io.njh.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,19 @@ public class ProjectService {
 
         // LOGIC WILL BE HERE - ESPECIALLY FOR UPDATE OPERATION. "IS USER THE OWNER TO UPDATE IT?"
 
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
+        }
 
-        return projectRepository.save(project);
     }
 
+    public Project findProjectByIdentifier(String projectId){
+
+
+        return projectRepository.findByProjectIdentifier((projectId));
+    }
 
 }
